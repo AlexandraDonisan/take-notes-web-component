7cce27cf720f2137a5bd061ca192d47abb235f95
Sandy <alexandra_donisan@yahoo.com>
2021-06-13 19:24:51 +0300

    Taking notes work

--- take-notes/src/TakeNotes.ts
+++ take-notes/src/TakeNotes.ts
@@ -3,24 +3,51 @@ import { html, css, LitElement, property } from 'lit-element';
 export class TakeNotes extends LitElement {
   static styles = css`
     :host {
-      display: block;
-      padding: 25px;
-      color: var(--take-notes-text-color, #000);
+    }
+
+    #take-notes-text-area {
+      // position: fixed;
+      // right: 0;
+      // margin-top: 30px;
+      // margin-right: 30px;
     }
   `;
 
-  @property({ type: String }) title = 'Hey there';
+  @property({ type: String }) title = "Let's study!";
 
-  @property({ type: Number }) counter = 5;
+  @property() notesList = ['Here are your notes:', '1', '2'];
 
-  __increment() {
-    this.counter += 1;
+  addToNotes() {
+    const note = this.getSelText()?.toString() || '';
+    if (note) {
+      this.notesList.push(note);
+      this.requestUpdate();
+    }
+  }
+
+  getSelText() {
+    let txt: Selection | null;
+    if (window.getSelection) {
+      txt = window.getSelection();
+    } else if (document.getSelection) {
+      txt = document.getSelection();
+    } else return;
+    return txt;
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <h2>${this.title}</h2>
+      <textarea
+        id="take-notes-text-area"
+        name="take-notes-text-area"
+        rows="35"
+        cols="50"
+      >
+        ${this.notesList.map(noteItem => `${noteItem}\n`)}
+      </textarea
+      >
+      <button @click=${this.addToNotes}>Take Note!</button>
     `;
   }
 }
